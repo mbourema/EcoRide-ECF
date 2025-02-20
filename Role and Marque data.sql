8efacdc135603edf31c81f342c8cad31fad82a74
mbourema <99417556+mbourema@users.noreply.github.com>
2025-02-20 23:39:56 +0100

    Add files via upload

---
+++ Role and Marque data.sql
@@ -0,0 +1,22 @@
+-- Requetes d'insertion pour la table Role
+
+INSERT INTO role (libelle) VALUES ('ROLE_ADMIN'); 
+INSERT INTO role (libelle) VALUES ('ROLE_EMPLOYE');
+INSERT INTO role (libelle) VALUES ('ROLE_CONDUCTEUR');
+INSERT INTO role (libelle) VALUES ('ROLE_PASSAGER');
+
+-- Requetes d'insertion pour la table Marque
+
+INSERT INTO marque (libelle) VALUES ('Alfa Romeo'); 
+INSERT INTO marque (libelle) VALUES ('Audi');
+INSERT INTO marque (libelle) VALUES ('BMW');
+INSERT INTO marque (libelle) VALUES ('Dacia');
+INSERT INTO marque (libelle) VALUES ('Fiat'); 
+INSERT INTO marque (libelle) VALUES ('Peugeot');
+INSERT INTO marque (libelle) VALUES ('Renault');
+INSERT INTO marque (libelle) VALUES ('Volkswagen');
+INSERT INTO marque (libelle) VALUES ('Mercedes'); 
+INSERT INTO marque (libelle) VALUES ('Ford');
+INSERT INTO marque (libelle) VALUES ('Nissan');
+INSERT INTO marque (libelle) VALUES ('Opel');
+INSERT INTO marque (libelle) VALUES ('Volvo');
